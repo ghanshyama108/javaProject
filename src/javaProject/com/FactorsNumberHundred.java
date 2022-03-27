afa2ce631076c2509774d88c2f91f4556b5c696c
ghanshyama108 <ghanshyama108@gmail.com>
2022-03-27 16:38:02 +0530

    firstprogram

---
+++ src/javaProject/com/FactorsNumberHundred.java
@@ -0,0 +1,24 @@
+package javaProject.com;
+
+
+	public class FactorsNumberHundred {
+		public static void main(String[] args) {
+			
+			for(int i=1; i<=100; i++)
+			{
+				System.out.println("Factors of a Number :"+i+" are");
+				
+				for(int j=1; j<=i; j++)
+				{
+					if(i%j==0)
+					{
+					System.out.println(""+j);
+					}
+				}
+			}
+		}
+
+	
+
+
+}
